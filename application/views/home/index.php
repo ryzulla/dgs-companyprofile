140189f72dd1be7ca11becd5a10eb726a828888c
Ryzulla <raryanapriansyah@gmail.com>
2018-10-09 11:04:42 +0700

    Update application/views/home/index.php

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -181,23 +181,7 @@
                             <img src="<?php echo base_url()?>assets/front/images/style1/header.png" alt="" data-bgposition="center center" data-no-retina>
                             <div class=""></div>
                             <!-- Layers -->
-                            <div class="tp-caption tp-resizeme text-ffb922 font-rubik font-weight-500 text-right center-mobile"
-                            data-x="['left','left','left','center']" data-hoffset="['-2','-2','-2','0']"
-                            data-y="['middle','middle','middle','middle']" data-voffset="['-118','-118','-118','-138']"
-                            data-fontsize="['20','20','20','16']"
-                            data-lineheight="['48','48','28','20']"
-                            data-width="full"
-                            data-height="none"
-                            data-whitespace="normal"
-                            data-transform_idle="o:1;"
-                            data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                            data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                            data-mask_in="x:0px;y:[100%];"
-                            data-mask_out="x:inherit;y:inherit;"
-                            data-start="700"
-                            data-splitin="none"
-                            data-splitout="none"
-                            data-responsive_offset="on"> WHO WE ARE? </div>
+                            
 
                             <div class="tp-caption tp-resizeme font-rubik font-weight-700 text-right center-mobile"
                             data-x="['left','left','left','center']" data-hoffset="['-2','-2','-2','0']"
